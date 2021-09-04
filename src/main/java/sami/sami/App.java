7004d0f2bf274ea1f696c9836a536dc3fd3941fd
rasw95 <90000520+rasw95@users.noreply.github.com>
2021-09-03 19:59:00 -0600

    Add files via upload

---
+++ src/main/java/sami/sami/App.java
@@ -0,0 +1,13 @@
+package sami.sami;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is maven!" );
+    }
+}
